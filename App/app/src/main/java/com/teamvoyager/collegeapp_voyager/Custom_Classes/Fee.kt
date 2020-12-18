dfeac4c1b72b90d832a5d438d350a734beba08c7
Anjali <anjalikumari13617@gmail.com>
2020-12-18 07:23:12 +0530

    implemented frontend and backend for canteen orders

---
+++ App/app/src/main/java/com/teamvoyager/collegeapp_voyager/Custom_Classes/Fee.kt
@@ -0,0 +1,12 @@
+package com.teamvoyager.collegeapp_voyager.Custom_Classes
+
+class Fee {
+
+    var event:String
+    var fee:String
+
+    constructor(event:String,fee:String){
+        this.event = event
+        this.fee = fee
+    }
+}
--- App/app/src/main/java/com/teamvoyager/collegeapp_voyager/activities/MainActivity.java
+++ App/app/src/main/java/com/teamvoyager/collegeapp_voyager/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.teamvoyager.collegeapp_voyager;
+package com.teamvoyager.collegeapp_voyager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.teamvoyager.collegeapp_voyager.R;
 
 public class MainActivity extends AppCompatActivity {
 
