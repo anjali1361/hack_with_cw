2cfe7bd3cfeeedc3a570581248c8840d5321e3f2
Shivam Anand <anand.shivam44@yahoo.com>
2020-12-18 23:32:05 +0530

    pdf api done

---
+++ backend/api.py
@@ -0,0 +1,46 @@
+import uvicorn
+import psycopg2
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, HttpUrl, parse_obj_as
+from typing import List, Set, Dict, Text
+app = FastAPI(debug=True)
+conn = psycopg2.connect(database="df912qntf815eh", user="qntcbpuyzvkslk",
+                        password="39b05f0abc02099fbed2afd0470964064380bc2a0a712cf125a939d4d3de5c49",
+                        host="ec2-3-210-23-22.compute-1.amazonaws.com", port="5432")
+cur = conn.cursor()
+
+
+
+class PDFModel(BaseModel):
+    id: int = 0
+    branch: str = "null"
+    # semester: str = "null"
+    category: str = "null"
+    url: str = "null"
+    name: str = "null"
+
+
+
+@app.get("/pdf/")
+async def pdf():
+    cur.execute("SELECT BRANCH, CATEGORY, URL, NAME  from PDF")
+    rows = cur.fetchall()
+    PDF_json: List[PDFModel] = []
+    i = 0
+    for row in rows:
+        PDF_item = PDFModel()
+        PDF_item.id = i
+        i = i+1
+        PDF_item.branch = row[0]
+        # PDF_item.semester
+        PDF_item.category = row[1]
+        PDF_item.url = row[2]
+        PDF_item.name = row[3]
+        PDF_json.append(PDF_item)
+
+    return PDF_json
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
